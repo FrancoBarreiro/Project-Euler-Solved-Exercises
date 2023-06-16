da0a40234117652aa745fdb7f754f2d40475ac0e
FrancoBarreiro <francco.barreiiro@gmail.com>
2023-06-16 19:19:08 -0300

    Resolución del Problema 1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,24 @@
+
+public class Ejercicio1 {
+    /**
+     * Problema 1: múltiplos de 3 y 5
+     * Si enumeramos todos los números naturales por debajo de 10 que son múltiplos de 3 o 5,
+     * obtenemos 3, 5, 6 y 9. La suma de estos múltiplos es 23.
+     * <p>
+     * Encuentre la suma de todos los múltiplos de 3 o 5 debajo del valor del parámetro proporcionado number.
+     */
+
+
+    public int sumaMultiplosDeTresYCinco(int numero) {
+        int resultado = 0;
+
+        for (int i = 0; i < numero; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                resultado += i;
+            }
+        }
+        System.out.println(""+resultado);
+        return resultado;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Problema 1 - Resuelto
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        ejercicio1.sumaMultiplosDeTresYCinco(10);
+        ejercicio1.sumaMultiplosDeTresYCinco(49);
+        ejercicio1.sumaMultiplosDeTresYCinco(1000);
+        ejercicio1.sumaMultiplosDeTresYCinco(8456);
+        ejercicio1.sumaMultiplosDeTresYCinco(19564);
+    }
+}
